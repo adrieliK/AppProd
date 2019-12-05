5030955d91c3c5e498a3bc5902e28cb5d318f189
adrieli <adrielikethin.dossantos@hotmail.com>
2019-12-05 13:16:49 -0200

    Quase concluido

--- app/src/main/java/com/example/aluno/myapplication2/MainActivity2.java
+++ app/src/main/java/com/example/aluno/myapplication2/MainActivity2.java
@@ -1,19 +1,12 @@
 package com.example.aluno.myapplication2;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-
-import com.orm.SugarDb;
 
 import static android.widget.Toast.LENGTH_SHORT;
 import static android.widget.Toast.makeText;
@@ -23,13 +16,6 @@ public class MainActivity2 extends AppCompatActivity {
     private EditText editQuantidade;
     private EditText editDTVal;
     private EditText editPreco;
-    private Button btnCadastrar;
-    private ImageButton btnFoto;
-    private Button buttonListar;
-    private TextView textNome;
-    private TextView textQuantidade;
-    private TextView textData;
-    private TextView textPreco;
 
 
 
@@ -39,13 +25,13 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
 
-        editNome = (EditText) findViewById(R.id.editNome);
-        editQuantidade = (EditText) findViewById(R.id.editQuantidade);
-        editDTVal = (EditText) findViewById(R.id.editDTVal);
-        editPreco = (EditText) findViewById(R.id.editPreco);
-        btnCadastrar = (Button) findViewById(R.id.btnCadastrar);
-        btnFoto = (ImageButton) findViewById(R.id.btnFoto);
-        buttonListar = (Button) findViewById(R.id.buttonListar);
+        editNome = findViewById(R.id.editNome);
+        editQuantidade = findViewById(R.id.editQuantidade);
+        editDTVal = findViewById(R.id.editDTVal);
+        editPreco = findViewById(R.id.editPreco);
+        Button btnCadastrar = findViewById(R.id.btnCadastrar);
+        ImageButton btnFoto = findViewById(R.id.btnFoto);
+        Button buttonListar = findViewById(R.id.buttonListar);
 
         btnFoto.setOnClickListener(new View.OnClickListener() {
             @Override
